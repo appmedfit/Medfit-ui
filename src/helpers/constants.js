be24f2a3a1ddb862235793b3a73040e24f25f722
charan-kanaparthi <kvenkatcharan@gmail.com>
2021-11-01 10:11:53 +0530

    added firebase url

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,13 +1,13 @@
-import { SignOut } from "../services/auth.service"
+import { SignOut } from "../services/auth.service";
 
-const constants={
-    baseUrl:"http://localhost:8080/api/v1/",
-    signIn:'user/signIn',
-    signUp:'user/signUp',
-    getStudents:'/student',
-    SignOut:'user/signOut',
-    getSpeciality:"specialty"
+const constants = {
+  baseUrl: "https://medfit-backend.herokuapp.com/api/v1/",
+  //"http://localhost:8080/api/v1/",
+  signIn: "user/signIn",
+  signUp: "user/signUp",
+  getStudents: "/student",
+  SignOut: "user/signOut",
+  getSpeciality: "specialty",
+};
 
-}
-
-export default constants
+export default constants;
