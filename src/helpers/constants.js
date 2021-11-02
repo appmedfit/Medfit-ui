a966f1e76098995ac4ec806a55a9797815f3f0aa
appmedfit <93030415+appmedfit@users.noreply.github.com>
2021-11-02 17:00:15 +0530

    Update constants.js

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,7 @@
 import { SignOut } from "../services/auth.service";
 
 const constants = {
-  baseUrl: "http://localhost:8080/api/v1/",
+  baseUrl: "https://medfit-backend.herokuapp.com/api/v1/",
   //      "https://medfit-backend.herokuapp.com/api/v1/",
   //"http://localhost:8080/api/v1/",
   signIn: "user/signIn",
