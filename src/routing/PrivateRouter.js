144b43b1ba8d52065b8ae804bbee513cafd4301a
appmedfit <appmedfit@gmail.com>
2022-07-15 17:31:55 +0530

    change

--- src/routing/PrivateRouter.js
+++ src/routing/PrivateRouter.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 export const PrivateRoute = ({ component: Component, ...rest }) => {
-    const {isAuthenticated} = useSelector((state)=>
-        ( sessionStorage.getItem('user')? JSON.parse(sessionStorage.getItem('user'))  :state.auth)
-    )
-    
-    return (
-        <Route {...rest} render={props => (
-            isAuthenticated
-                ? <Component {...props} />
-                : <Redirect to={{ pathname: '/'}} />
-        )} />
-    )
-    }
+  const { isAuthenticated } = useSelector((state) =>
+    sessionStorage.getItem("user")
+      ? JSON.parse(sessionStorage.getItem("user"))
+      : state.auth
+  );
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/" }} />
+        )
+      }
+    />
+  );
+};
